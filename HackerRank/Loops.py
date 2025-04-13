606bafed5370287677564abeff3077c8c1a9f35e
Vairamuthu <vetrivaira@gmail.com>
2025-04-13 23:31:52 +0530

    Loops

---
+++ HackerRank/Loops.py
@@ -0,0 +1,13 @@
+#Task
+#The provided code stub reads an integer, , from STDIN. For all non-negative integers , print .
+#Example
+#The list of non-negative integers that are less than  is . Print the square of each number on a separate line.
+#0
+#1
+#4
+
+if __name__ == '__main__':
+    n = int(input())
+
+for i in range (n):
+    print(i*i)
